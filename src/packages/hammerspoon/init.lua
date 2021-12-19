8923d1fee057c6cb3ac7abca8c771f5cbb0d9b65
Jeff Martin <nodeselector@github.com>
2021-12-19 09:54:33 -0800

    hs: add cli

--- src/packages/hammerspoon/init.lua
+++ src/packages/hammerspoon/init.lua
@@ -1,1 +1,5 @@
-hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
+-- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
+hs.loadSpoon('ControlEscape'):start()
+
+-- Install the CLI tool for Hammerspoon, doc at https://www.hammerspoon.org/docs/hs.ipc.html#cliInstall
+hs.ipc.cliInstall()
