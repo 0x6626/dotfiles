598388a45c4fb78a94e96b3916bcb79a7ea92473
Jeff Martin <nodeselector@github.com>
2021-12-22 01:11:44 -0800

    add more fzf funcs

--- src/packages/zsh/.zsh/functions.zsh
+++ src/packages/zsh/.zsh/functions.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+source $HOME/.zsh/functions
 
 function open_repo_in_code() {
   IFS=$'\n' files=($(ghq list -p | fzf --query="$1" --multi --select-1 --exit-0))
@@ -6,4 +7,22 @@ function open_repo_in_code() {
 }
 
 zle -N open_repo_in_code
-bindkey "^g" open_repo_in_code
+bindkey "^g^o" open_repo_in_code
+
+# fshow - git commit browser
+function fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr by %C(yellow)%ae" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
+
+zle -N fshow
+bindkey "^g^b" fshow
+
+zle -N dot_reload
+bindkey "^r^r" dot_reload
